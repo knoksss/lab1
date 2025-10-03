72cf97682c530a376c7a93eacc9bf5e778986dda
knoksss <dashak260207@gmail.com>
2025-10-03 20:56:47 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,20 +1,15 @@
-from src.power import power_function
-from src.constants import SAMPLE_CONSTANT
+
+from power import power_function
 
 
 def main() -> None:
-    """
-    Обязательнная составляющая программ, которые сдаются. Является точкой входа в приложение
-    :return: Данная функция ничего не возвращает
-    """
 
-    target, degree = map(int, input("Введите два числа разделенные пробелом: ").split(" "))
+    entrance_str = input('Введите выражение в обратной польской нотации: ')
 
-    result = power_function(target=target, power=degree)
+    result = power_function(entrance_str)
 
-    print(result)
+    print('Посчитанное выражение:', result)
 
-    print(SAMPLE_CONSTANT)
 
 if __name__ == "__main__":
     main()
