04ddbc280da355e20cf84b861fb66e890c32a511
knoksss <dashak260207@gmail.com>
2025-10-29 18:12:17 +0300

    update

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 
-from power import power_function
+from power import main_function
 
 
 def main() -> None:
     entrance_str = input('Введите выражение в обратной польской нотации: ')
-    result = power_function(entrance_str)
+    result = main_function(entrance_str)
     print('Посчитанное выражение:', result)
 
 
